9a19185d1991ebf717ffbb0d15e6e7653ee4df62
Majidzadeh <hashpragmaonce@gmail.com>
2020-05-15 22:08:00 +0430

    Add "Set Wallpaper" button to MainActivity

--- MaterialsLiveWallpaper/app/src/main/java/com/reminimalism/materialslivewallpaper/MainActivity.java
+++ MaterialsLiveWallpaper/app/src/main/java/com/reminimalism/materialslivewallpaper/MainActivity.java
@@ -2,7 +2,11 @@ package com.reminimalism.materialslivewallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnSetWallpaperClick(View view)
+    {
+        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(
+                WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                new ComponentName(this, MaterialsWallpaperService.class)
+        );
+        startActivity(intent);
     }
 }
